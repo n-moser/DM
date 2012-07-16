45822b0196f83db707d0c0faeaec81906f4fc002
Nicolas Moser <nico@moser-home.de>
2012-07-16 11:30:20 +0200

    Web Layout

---
+++ com.moser.dm2.ui.web/conf/web/js/app.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2012 Nicolas Moser.
+ */
+
+/**
+ * 
+ * @author Nicolas Moser
+ */
+function menuClicked(var name) {
+
+	alert(name);
+	
+	swtich(name) {
+		
+		case 'NEW': {
+			
+		}
+		
+		case 'OPEN': {
+			
+		}
+		
+		case 'STAT': {
+			
+		}
+		
+	}
+	
+}
